c940cdcd755169311c950b863fe7467912570449
Pan Cheng <pchengeducation@hotmail.com>
2018-07-03 01:23:47 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 #write your code here
-number = 10
-def countdown (number)
 
+def countdown (number)
+number = 10
   while number > 0
     puts "#{number} SECOND(S)!"
     wait = 5
@@ -12,7 +12,7 @@ def countdown (number)
 end
 
 def countdown_with_sleep(wait)
-  while wait < 5
+  while wait < 5 
     wait -= 1
   end
 end
