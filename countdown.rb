f1b8e2bce32ada52fad8e41774b93e67f2dbb389
Pan Cheng <pchengeducation@hotmail.com>
2018-07-03 01:23:25 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,15 @@ def countdown (number)
 
   while number > 0
     puts "#{number} SECOND(S)!"
+    wait = 5
+    countdown_with_sleep(wait)
     number -= 1
   end
   puts "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(wait)
+  while wait < 5 
+    wait -= 1
+  end
 end
